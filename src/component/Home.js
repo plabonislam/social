f59ba9c42aa696d82cae01f204bd18248258e3fc
shahnurislamplabon <shahnurislamplabon>
2020-07-22 05:47:12 +0600

    delete && fav Updated

--- src/component/Home.js
+++ src/component/Home.js
@@ -10,50 +10,85 @@ import { reducer, posts } from "../reducers/userReducer";
 import { userContext } from "../App";
 export const LikeContext = createContext();
 
-const Item = ({ _id, title, body, likes, image, postedBy: { name } }) => {
+const Item = ({ _id, title, body,favourite, likes, image, postedBy: { name } }) => {
   const { state, dispatch } = useContext(userContext);
   const { state2, dispatch2, posts } = useContext(LikeContext);
-  console.log(state, "faltttttttttttttttt");
+  
 
   const likePost = (id) => {
-    code("like", id);
+    code("like", id,"PUT");
   };
 
   const dislikePost = (id) => {
-    code("dislike", id);
+    code("dislike", id,"PUT");
+  };
+ const fav =(id)=>{
+   code("fav",id,"PUT");
+ }
+
+
+  const unfav = (id) => {
+    code("unfav", id,"PUT");
   };
 
-  const code = (url, id) => {
+
+  const del=(id)=>{
+    code ("del",id,"delete");
+  }
+  const code = (url, id,method) => {
     let auth = localStorage.getItem("auth");
-    axios
-      .put(
-        `http://localhost:3000/posts/${url}`,
-        { postId: id },
-        {
-          headers: {
-            Authorization: `Bearer ${auth}`,
-          },
-        }
-      )
+    axios({
+      method: method,
+      url: `http://localhost:3000/posts/${url}`,
+      data: { postId: id },
+
+      headers: {
+        Authorization: `Bearer ${auth}`,
+      },
+    })
       .then((response) => {
-        console.log(response.data.likes);
-        const all = posts.map((item) => {
+        
+        let all = posts;
+        console.log(method);
+        if (method === "delete"){
+          alert(response.data.mess);
+          if ("Not An Author" !== response.data.mess)
+           all = posts.filter((word) => word._id != id);
+      }
+
+      else{
+
+         all = posts.map((item) => {
           if (id == item._id) {
             console.log("i am groot");
-            return response.data;
+            //   continue;
+             return response.data;
           } else {
             return item;
           }
         });
+      }
         dispatch2({ type: "POST", payload: all });
       })
       .catch((err) => console.log(err));
   };
 
   return (
-    <div
-      className="card home" >
-      <h5>{name}</h5>
+    <div className="card home">
+      <h5>
+        {name}
+        <span style={ {float:"right"}}>
+          
+          <i
+            className="material-icons"
+            onClick={() => {
+              del(_id);
+            }}
+          >
+            delete_forever
+          </i>
+        </span>
+      </h5>
       <div className="card-image hcard">
         <img src={image} />
         <span className="card-title">{title}</span>
@@ -61,7 +96,25 @@ const Item = ({ _id, title, body, likes, image, postedBy: { name } }) => {
 
       <div className="card-content">
         <p>
-          <i className="material-icons">favorite_border</i>
+          {favourite.includes(state._id) ? (
+            <i
+              className="material-icons"
+              onClick={() => {
+                unfav(_id);
+              }}
+            >
+              favorite
+            </i>
+          ) : (
+            <i
+              className="material-icons"
+              onClick={() => {
+                fav(_id);
+              }}
+            >
+              favorite_border
+            </i>
+          )}
           {likes.includes(state._id) ? (
             <i
               className="material-icons"
